f022f7c12eecdb9c498d3f44b7239a57724fdafc
Christian Geib <christian.geib79@gmail.com>
2018-08-24 08:40:26 +0100

    second commit still not adding permanent list, commented out attempts to remedy this

--- js/app.js
+++ js/app.js
@@ -5,29 +5,30 @@ const form = document.querySelector('#new-item-form');
 form.addEventListener('submit', handleFormSubmit);
 });
 //------
+
 const handleFormSubmit = function(event){
   event.preventDefault();
+
+  // const newItem = document.createElement("li");
+  // newListItem.textContent = `${event.target.title.value}`;
+  // newListItem.classPrimary.add("title");
+  // const list = document.querySelector('ul');
+  // list.appendChild(newListItem);
+  //
+  // const newItem = document.createElement("li");
+  // newListItem.textContent = `${event.target.author.value}`;
+  // newListItem.classPrimary.add("author");
+  // const list = document.querySelector('ul');
+  // list.appendChild(newListItem);
+  //
+  // const newItem = document.createElement("li");
+  // newListItem.textContent = `${event.target.category.value}`;
+  // newListItem.classPrimary.add("category");
+  // const list = document.querySelector('ul');
+  // list.appendChild(newListItem);
 
   const resultForm = document.querySelector('#reading-list')
   resultForm.textContent = `${event.target.title.value} ${event.target.author.value} ${event.target.category.value}`
 
   document.querySelector("#new-item-form").reset();
 }
-
-// const newItem = document.createElement("li");
-// newListItem.textContent = `${event.target.title.value}`;
-// newListItem.classPrimary.add("title");
-// const list = document.querySelector('ul');
-// list.appendChild(newListItem);
-//
-// const newItem = document.createElement("li");
-// newListItem.textContent = `${event.target.author.value}`;
-// newListItem.classPrimary.add("author");
-// const list = document.querySelector('ul');
-// list.appendChild(newListItem);
-//
-// const newItem = document.createElement("li");
-// newListItem.textContent = `${event.target.category.value}`;
-// newListItem.classPrimary.add("category");
-// const list = document.querySelector('ul');
-// list.appendChild(newListItem);
